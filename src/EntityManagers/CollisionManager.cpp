35e3cdb3a369223f4e5b2b3cc3543fa9d27216dd
Fatihcil16 <fatihcil.2001@gmail.com>
2021-11-08 18:06:17 +0300

    collisions updated

--- src/EntityManagers/CollisionManager.cpp
+++ src/EntityManagers/CollisionManager.cpp
@@ -6,40 +6,43 @@ void CollisionEntityRecVec(Entity *instance, std::vector<Rectangle> *collisions,
     //TODO: better collision detection
      for (Rectangle col : *collisions)
     {
-        if (col.y <= instance->position.y &&
-           col.y + col.height >= instance->position.y)
+        float borderleft = col.x + col.width + instance->entityrec.width;
+        float bordertop = col.y + col.height + instance->entityrec.height;
+        
+        if (col.y < instance->position.y &&
+           col.y + col.height > instance->position.y)
         {
-            if (col.x >= instance->position.x &&
+            if (borderleft <= instance->position.x &&
+                borderleft > instance->position.x + instance->velocity.x * dt)
+            {
+                instance->hitOnWall = true;
+                instance->velocity.x = 0.0f;
+                instance->position.x = borderleft;
+            }
+            else if (col.x >= instance->position.x &&
                 col.x < instance->position.x + instance->velocity.x * dt)
             {
                 instance->hitOnWall = true;
                 instance->velocity.x = 0.0f;
                 instance->position.x = col.x;
             }
-            else if (col.x + col.width + instance->entityrec.width <= instance->position.x &&
-                col.x + col.width + instance->entityrec.width > instance->position.x + instance->velocity.x * dt)
-            {
-                instance->hitOnWall = true;
-                instance->velocity.x = 0.0f;
-                instance->position.x = col.x + col.width + instance->entityrec.width;
-            }
         }
-
-        if (col.x <= instance->position.x &&
-            col.x + col.width >= instance->position.x - instance->entityrec.width)
+        
+        if (col.x < instance->position.x &&
+            col.x + col.width > instance->position.x - instance->entityrec.width)
         {
-            if (col.y >= instance->position.y &&
+            if (bordertop <= instance->position.y &&
+                bordertop > instance->position.y + instance->velocity.y * dt)
+            {
+                instance->velocity.y = 0.0f;
+                instance->position.y = bordertop;
+            }
+            else if (col.y >= instance->position.y &&
                 col.y < instance->position.y + instance->velocity.y * dt)
             {
-            instance->isGrounded = true;
-            instance->velocity.y = 0.0f;
-            instance->position.y = col.y;
-            }
-            else if (col.y + col.height <= instance->position.y &&
-                col.y + col.height > instance->position.y + instance->velocity.y * dt)
-            {
-            instance->velocity.y = 0.0f;
-            instance->position.y = col.y + col.height;
+                instance->isGrounded = true;
+                instance->velocity.y = 0.0f;
+                instance->position.y = col.y;
             }
         }
         //logs
